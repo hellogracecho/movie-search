82a350c5ec297a39ac7db755241e8567b7c806ee
Grace Cho <hellogracec@gmail.com>
2024-07-25 16:33:16 -0700

    add Google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,63 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const aspectRatio = require('@tailwindcss/aspect-ratio')
+
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      primary: '#00a69c',
-      'off-white': '#fff',
-      black: '#000'
+      primary: '#efcd59',
+      secondary: '#42b883',
+      warning: '#e83b9c',
+      white: '#efede6',
+      black: '#1f1f1f'
     },
-    extend: {}
+    container: {
+      center: true,
+      screens: {
+        sm: `640px`,
+        md: `768px`,
+        lg: `1024px`,
+        xl: `1280px`,
+        '2xl': `calc(1536px + 4rem)`
+      }
+    },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px'
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans]
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            color: 'var(--color-neutral-400)',
+            iframe: {
+              width: '100%',
+              height: '100%',
+              aspectRatio: '16/9'
+            },
+            a: {
+              color: 'var(--color-secondary-500)',
+              '&:hover': {
+                color: 'var(--color-secondary-200)'
+              }
+            }
+          }
+        }
+      }
+    }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/aspect-ratio')]
 }
